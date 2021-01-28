0b8fd0a691f10a09b55dfd54c616dfeebc4012b0
tiegrun <tiko3020@gmail.com>
2021-01-28 05:00:54 -0500

    events test

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,62 @@
 import React, { Component } from "react";
 
+
 class Event extends Component {
+
+  constructor() {
+    super();
+ 
+    this.state = {
+      showDetails: false,
+      query: 'Munich',
+      suggestions: []
+    }
+  }
+
+  handleShowDetails = () => {
+
+    if (this.state.showDetails === false) {
+      this.setState({ showDetails: true });
+    } else {
+      this.setState({ showDetails: false });
+    }
+
+  };
+
   render() {
-    return <div></div>;
+    
+    const EventItem = this.props.events.map(((item)=>{
+      [item.summary].join(" ");
+      return item.summary;
+    }));
+
+    const EventDescript = this.props.events.map(((item)=>{
+      [item.description].join(" ");
+      return item.description;
+    }))
+
+
+    return (
+      <div className="event">
+        <div className="eventOverview">
+          <p className="eventName">
+            {EventItem}
+          </p>         
+          <button className="detailsBtn" onClick={() => this.handleShowDetails()}>
+            show details
+          </button>
+        </div>
+
+        {this.state.showDetails && (
+          <div className="eventDetails">
+            <p className="eventDetailsDescription">
+              {EventDescript}
+            </p>
+          </div>
+        )}
+      </div>
+    );
   }
 }
+
 export default Event;
---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+import { mockData } from "../mock-data";
+
+describe('<Event /> component', () => {
+
+  let EventWrapper;
+
+  beforeAll(() => {
+    EventWrapper = shallow(<Event events={mockData}/>);
+  });
+
+  test('test that componet is rendered', () => {
+    expect(EventWrapper).toHaveLength(1);
+  });
+
+  test('test that event wrapping div is rendered', () => {
+    expect(EventWrapper.find('.event')).toHaveLength(1);
+  });
+
+  test('test that event wrapping div just shows event__Overview', () => {
+    expect(EventWrapper.find('.event').children()).toHaveLength(1);
+  });
+
+  test('test that eventOverview is rendered', () => {
+    expect(EventWrapper.find('.eventOverview')).toHaveLength(1);
+  });
+
+  test('test that eventOverview children are rendered', () => {
+    expect(EventWrapper.find('.eventOverview').children()).toHaveLength(2);
+  });
+
+  test('test that eventDetails children are rendered', () => {
+    EventWrapper.setState({
+      showDetails: true
+    });
+    expect(EventWrapper.find('.eventDetailsDescription')).toHaveLength(1);
+  });
+
+  test('test that show/hide details button is rendered', () => {
+    expect(EventWrapper.find('.detailsBtn')).toHaveLength(1);
+  });
+
+  test('click on button should show details', () => {
+    EventWrapper.setState({
+      showDetails: false
+    });
+    EventWrapper.find('.detailsBtn').simulate('click');
+    expect(EventWrapper.state('showDetails')).toBe(true);
+  });
+})
