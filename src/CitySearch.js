33532bb69033e6c200b8312dcfbc7e56f50edfaa
tiegrun <tiko3020@gmail.com>
2021-02-20 04:18:43 -0500

    info allert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
 
   state = {
     query: '',
     suggestions: [],
-    showSuggestions: false
+    showSuggestions: false,
+    infoText:''
   }
+
+  // handleInputChanged = (event) => {
+  //   const value = event.target.value;
+  //   const suggestions = this.props.locations.filter((location) => {
+  //     return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+  //   });
+  //   this.setState({
+  //     query: value,
+  //     suggestions,
+  //   });
+  // };
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({showSuggestions:true});
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText:''
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
@@ -32,6 +54,7 @@ class CitySearch extends Component {
    
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,35 +1,52 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
 
   state = {
-    eventCount:  32
-    
+    eventCount:  32,
+    errorText:''
   }
 
   handleInputChanged = (event) => {
-    const value = parseInt(event.target.value);
-    
-    if(value >= 1  && value <= 250){
-      this.setState({ 
-        eventCount: value 
-      });
 
-    }
+    if(parseInt(event.target.value)){
+      
+      const value = parseInt(event.target.value);
+    
+      if(value >= 1  && value <= 250){
+        this.setState({ 
+          eventCount: value,
+          errorText:''
+        });
+
+      }
+      else{
+        // alert("please enter a valid number between 0 and 250");
+        this.setState({
+          eventCount: value,
+          errorText: 'please enter a valid number between 1 and 250',
+        });
+      }
+
+      this.props.updateEvents(this.props.locations, value);
+
+   }
     else{
-      alert("please enter a valid number between 0 and 250")
+
+      this.setState({ 
+        eventCount: "",
+        errorText: 'please enter a valid number between 1 and 250'
+      });
+      
     }
-    
-    this.props.updateEvents(this.props.locations, value);
-    
   }
 
   render() {
     
-    // const {locations} = 
-    // console.log(this.props.locations);
     return(
       <div className="numberOfEvents">
+        <ErrorAlert text={this.state.errorText} />
         <label>Number of Events: </label>
         <input type="text" id="numberOfEventsInput" value={this.state.eventCount} onChange={this.handleInputChanged}/>
       </div>
