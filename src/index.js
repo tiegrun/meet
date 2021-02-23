6646f79dbea0d23e8aaaf4b6eab7bd7ff8a01daa
tiegrun <tiko3020@gmail.com>
2021-02-23 02:39:24 -0500

    service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
@@ -17,6 +17,8 @@ atatus.config('d40143fef1e445ff94524e2f7f1cc402').install();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+serviceWorker.unregister();
 
 // atatus.notify(new Error('Test Atatus Setup'));
 reportWebVitals();
